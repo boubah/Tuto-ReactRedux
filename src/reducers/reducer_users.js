56cf5065edc3f3cc41f6554cbb70bdab94526462
Aboubacar <aboubacar.camara@sis-marches.com>
2018-06-13 15:13:12 +0200

    Implémentation UserReducer

---
+++ src/reducers/reducer_users.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {name: "Sarah Croche", id:1, role: "ROOT", actif: "OUI"},
+        {name: "Eléonore Labanquise", id:2, role: "USER", actif: "NON"},
+        {name: "Emma Karena", id:3, role: "ADMIN", actif: "OUI"},
+        {name: "Jean Bonnot", id:4, role: "ADMIN", actif: "OUI"}
+    ]
+};
